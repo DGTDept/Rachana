b9955bd2f118132a73d40d71c428c6c764069f91
Virakden <virakden1717@gmail.com>
2023-11-30 10:43:09 +0000

    feat: Update implement on combo box

--- src/cgds.bundle.js
+++ src/cgds.bundle.js
@@ -1,5 +1,7 @@
 
 import * as bootstrap from 'bootstrap';
 
+import "./_componentjs/combobox"
+
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
