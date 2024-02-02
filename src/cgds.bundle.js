86ea4f2125c2fe1061b1f91b238d20962c281389
pr0meth4us <phearaneron.soeung@gmail.com>
2024-02-02 17:34:51 +0000

    fix: format style on component

--- src/cgds.bundle.js
+++ src/cgds.bundle.js
@@ -1,8 +1,9 @@
 
 import * as bootstrap from 'bootstrap';
-import "./_patternjs/date_picker"
-
 import "./_componentjs/combobox"
+import "./_patternjs/date_picker"
+import "./_patternjs/phonenumber"
+
 
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
